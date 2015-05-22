ea0242e6895fb59016a8b927210997ce426ec674
Henry  Kremerov <HenryK@iMac.local>
2015-05-22 17:06:44 -0400

    first commit

---
+++ CatYears/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  CatYears
+//
+//  Created by Henry  Kremerov on 1/4/15.
+//  Copyright (c) 2015 Henry  Kremerov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var age: UITextField!
+
+    @IBOutlet var Output: UILabel!
+    @IBAction func Convert(sender: AnyObject) {
+        
+        var enteredage=age.text.toInt()
+        var catyears=enteredage! * 7
+        Output.text="The age is \(catyears) "
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
